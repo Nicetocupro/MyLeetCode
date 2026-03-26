46e3b258ae8fa31de2bd260aa7514257e962c1c9
Nicetocupro <149050953+Nicetocupro@users.noreply.github.com>
2026-03-26 19:43:20 +0800

    Time: 0 ms (100%), Space: 70.1 MB (70.71%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,26 +2,14 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) 
     {
-        return Msum(nums , 0 , nums.size() - 1)[2];
-    }
+        // 动态规划
+        int pre = 0, maxNum = nums[0];
+        for(const auto x : nums)
+        {
+            pre = max(x, pre + x);
+            maxNum = max(maxNum, pre);
+        }
 
-    vector<int> Msum(vector<int>& nums, int l , int r)
-    {
-        int m = (l + r) / 2;
-        if(l == r)
-        {
-            return { nums[l], nums[l], nums[l], nums[l] };
-        }
-        else
-        {
-            vector<int> larray , rarray , marray(4);
-            larray = Msum(nums , l , m);
-            rarray = Msum(nums , m + 1 , r);
-            marray[0] = max(larray[0], larray[3] + rarray[0]); // Lsum
-            marray[1] = max(rarray[1], rarray[3] + larray[1]); // rsum
-            marray[2] = max(max(larray[2], rarray[2]), larray[1] + rarray[0]); // msum
-            marray[3] = larray[3] + rarray[3]; // isum
-            return marray;
-        }
+        return maxNum;
     }
 };
