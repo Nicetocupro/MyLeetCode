99a7975ed23a48c50dc42ba10575d8b761adabee
Nicetocupro <149050953+Nicetocupro@users.noreply.github.com>
2026-04-02 15:42:33 +0800

    Time: 3 ms (22.3%), Space: 20.5 MB (21.88%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -1,28 +1,34 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<pair<int, int>> dp;
+        std::set<int> mySets;
+
         for(int i = 0; i < matrix.size(); i++)
         {
+            bool row = false;
             for(int j = 0; j < matrix[0].size(); j++)
             {
                 if(matrix[i][j] == 0)
                 {
-                    dp.push_back(std::make_pair(i, j));
+                    row = true;
+                    mySets.insert(j);
+                }
+            }
+
+            if(row)
+            {
+                for(int j = 0; j < matrix[0].size(); j++)
+                {
+                    matrix[i][j] = 0;
                 }
             }
         }
 
-        for(auto pair: dp)
+        for(auto mySet: mySets)
         {
-            for(int i = 0; i < matrix[0].size(); i++)
-            {
-                matrix[pair.first][i] = 0;
-            }
-
             for(int i = 0; i < matrix.size(); i++)
             {
-                matrix[i][pair.second] = 0;
+                matrix[i][mySet] = 0;
             }
         }
     }
