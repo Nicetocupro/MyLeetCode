86a8234723435287d1e06e9f50e4b6d6cf18df38
Nicetocupro <149050953+Nicetocupro@users.noreply.github.com>
2026-04-10 23:40:38 +0800

    Time: 26 ms (6.85%), Space: 14.9 MB (5.28%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // 快慢指针 或者 set
+        std::set<ListNode*> mySet;
+        ListNode* temp = head;
+        while(temp != nullptr)
+        {
+            if(!mySet.contains(temp))
+            {
+                mySet.insert(temp);
+            }
+            else
+            {
+                return true;
+            }
+            temp = temp->next;
+        }
+        return false;
+    }
+};
